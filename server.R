ae0558afcedbcc7ce437031d2dbf61a7cf83d270
CatherineLi <CatherineLi@users.noreply.github.com>
2015-04-12 20:14:17 -0700

    Update server.R

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ shinyServer(
                          input$expense7)
                      
                      total
-                     })
+             })
              
              output$balance<-renderPrint({
                      total<-sum(input$expense1, input$expense2, input$expense3, 
@@ -18,8 +18,19 @@ shinyServer(
                                 input$expense7)
                                       
                      input$budget-total
-                    })
+             })
+             
+             output$pieChart<-renderPlot({
+                     total<-sum(input$expense1, input$expense2, input$expense3, 
+                                input$expense4, input$expense5, input$expense6,
+                                input$expense7)
+                     
+                    pie(c(total, (input$budget-total)), labels=c("spending", "remaining"),
+                        col=rainbow(2))
+                     
+             })
                                   
                                  
      }    
  )
+       
